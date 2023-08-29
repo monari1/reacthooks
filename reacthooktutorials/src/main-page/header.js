0fe26872c71cb0f503d84655a495eb95586db413
monari1 <francisnyasinga@gmail.com>
2023-08-29 15:59:57 +0300

    useEffect intro

---
+++ reacthooktutorials/src/main-page/header.js
@@ -0,0 +1,27 @@
+import { useEffect, useState, React } from 'react';
+
+function Header(props) {
+    const [resourceType, setResourceType] = useState('Post')
+
+    function post(){
+        setResourceType("Hello world");
+    }
+    function users(){
+        setResourceType("users");
+    }
+    function comments(){
+        setResourceType("comments");
+    }
+    return(
+        <>
+        <div>
+            <button onClick={post}>Post </button>
+            <button onClick={users}>Users </button>
+            <button onClick={comments}>Comments</button>
+        </div>
+        <h1>{resourceType}</h1>
+        </>
+    )
+}
+
+export default Header;
--- reacthooktutorials/src/main-page/index.js
+++ reacthooktutorials/src/main-page/index.js
@@ -1,5 +1,6 @@
 import './main-page.css'
 import React, {useState} from 'react';
+import Header from './header';
 function countInitial(){
     console.log('run function');
     return 5;
@@ -7,22 +8,29 @@ function countInitial(){
 function App() {
 
 
-    const [count, setCount] = useState(()=> countInitial());
+    const [state, setState] = useState( {count: 4, theme: 'blue'});
 
+    const count = state.count;
+    const theme = state.theme;
     function Add(){
-        setCount(prevCount => prevCount + 1);
+        setState(prevState =>{return {...prevState, count:prevState.count +1}})
+        // setState(prevState => {...prevState, prevState.count+1});
     }
 
-    function Substract(){
-        setCount(prevCount => prevCount + 1);
+    function Sub(){
+        // setCount(prevCount => prevCount + 1);
+        setState(prevState =>{return {...prevState, count:prevState.count -1}})
+
     }
 
 
 
     return(
         <div>
-        <button onClick={Substract}>-</button>
+            <div><Header/></div>
+        <button onClick={Sub}>-</button>
         <span>{count}</span>
+        <span>{theme}</span>
         <button onClick={Add}>+</button>
         
         </div>
